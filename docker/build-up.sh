487dc420493b2c4a15b0b2a5dc2f13e1c20f3ac1
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-05-18 17:19:59 +0900

    [change] container name 'bayopt'

--- docker/build-up.sh
+++ docker/build-up.sh
@@ -2,4 +2,4 @@
 # build
 docker build -t cuda-python:latest .
 # up
-docker run -d -it --rm -v $(PWD)/../bayopt:/var/project/bayopt --name cuda-python_1 cuda-python:latest /bin/bash
+docker run -d -it --rm -v $(PWD)/../bayopt:/var/project/bayopt --name bayopt cuda-python:latest /bin/bash
