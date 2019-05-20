eeb0f42f9598ef4ebe01a18cf515a605c74c39a8
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-05-20 18:52:13 +0900

    [new] date methods; 1. return current time, string format

---
+++ bayopt/clock/clock.py
@@ -0,0 +1,7 @@
+import datetime
+from pytz from timezone
+
+def now_str():
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    now = now.astimezone(timezone('Asia/Tokyo'))
+    return now.strftime('%Y-%m-%d %H:%M:%S')
