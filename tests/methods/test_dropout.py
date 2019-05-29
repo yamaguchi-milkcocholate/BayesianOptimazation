201f9a559ef000d6ad1cdc1387a1b449332143ba
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-05-29 13:33:46 +0900

    [update] optimization works;

--- tests/methods/test_dropout.py
+++ tests/methods/test_dropout.py
@@ -8,19 +8,19 @@ class TestDropout(unittest.TestCase):
     def setUp(self) -> None:
         def f(x):
             x0, x1 = x[:, 0], x[:, 1]
-            f0 = np.log(10.5 - x0) + 0.1 * np.sin(15 * x0)
-            f1 = np.cos(1.5 * x0) + 0.1 * x0
-            return (1 - x1) * f0 + x1 * f1
+            return -0.1*(x0**2+x1**2-16)**2 + 10*np.sin(3*x0)
 
-        bounds = [{'name': 'x0', 'type': 'continuous', 'domain': (0, 10), 'dimensionality': 1},
-                  {'name': 'x1', 'type': 'continuous', 'domain': (0, 1), 'dimensionality': 1},
-                  {'name': 'x2', 'type': 'continuous', 'domain': (0, 1), 'dimensionality': 1}]
+        bounds = [{'name': 'x0', 'type': 'continuous', 'domain': (-3, 3), 'dimensionality': 1},
+                  {'name': 'x1', 'type': 'continuous', 'domain': (-3, 3), 'dimensionality': 1},
+                  {'name': 'x2', 'type': 'continuous', 'domain': (-3, 3), 'dimensionality': 1}
+                  ]
         self.method = Dropout(
             f=f, domain=bounds, subspace_dim_size=2
         )
 
     def test_example(self):
         self.method.run_optimization(max_iter=30)
+        self.method.save_report(report_file='report.txt')
         self.assertTrue(True)
 
     def test_check_domain(self):
