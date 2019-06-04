7273de4325de0d8fa460496445a912720a0473e5
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-06-04 19:34:54 +0900

    [update] add new batch exec files

--- bayopt/examples/Schwefel's function/dropout30D.py
+++ bayopt/examples/Schwefel's function/dropout30D.py
@@ -1,6 +1,5 @@
 from bayopt.methods.dropout import Dropout
-from bayopt.objective_examples.experiments import GaussianMixtureFunction
-import numpy as np
+from bayopt.objective_examples.experiments import SchwefelsFunction
 
 domain = [{'name': 'x0', 'type': 'continuous', 'domain': (1, 4), 'dimensionality': 1},
           {'name': 'x1', 'type': 'continuous', 'domain': (1, 4), 'dimensionality': 1},
@@ -34,10 +33,21 @@ domain = [{'name': 'x0', 'type': 'continuous', 'domain': (1, 4), 'dimensionality
           {'name': 'x29', 'type': 'continuous', 'domain': (1, 4), 'dimensionality': 1},
           ]
 
-dim = len(domain)
-fill_in_strategy = 'copy'
-f = GaussianMixtureFunction(dim=dim, mean_1=2, mean_2=3)
-X = np.array([np.full(dim, 1)])
-method = Dropout(f=f, domain=domain, subspace_dim_size=1, fill_in_strategy=fill_in_strategy, maximize=True,
-                 )
-method.run_optimization(max_iter=500, eps=0)
+
+for i in range(30):
+
+    fill_in_strategy = 'random'
+    f = SchwefelsFunction()
+    method = Dropout(f=f, domain=domain, subspace_dim_size=5, fill_in_strategy=fill_in_strategy, maximize=False)
+    method.run_optimization(max_iter=500)
+
+    fill_in_strategy = 'copy'
+    f = SchwefelsFunction()
+    method = Dropout(f=f, domain=domain, subspace_dim_size=1, fill_in_strategy=fill_in_strategy, maximize=False,
+                     )
+    method.run_optimization(max_iter=500, eps=0)
+
+    fill_in_strategy = 'mix'
+    f = SchwefelsFunction()
+    method = Dropout(f=f, domain=domain, subspace_dim_size=5, fill_in_strategy=fill_in_strategy, maximize=False, mix=0.5)
+    method.run_optimization(max_iter=500)
