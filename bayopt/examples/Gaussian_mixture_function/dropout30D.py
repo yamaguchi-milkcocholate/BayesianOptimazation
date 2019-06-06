a5d623c25023fd93a07a1bb82306d31a3df815ad
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-06-06 12:08:47 +0900

    [update] experiment's setting

--- bayopt/examples/Gaussian_mixture_function/dropout30D.py
+++ bayopt/examples/Gaussian_mixture_function/dropout30D.py
@@ -40,7 +40,7 @@ for i in range(30):
     fill_in_strategy = 'random'
     f = GaussianMixtureFunction(dim=dim, mean_1=2, mean_2=3)
     method = Dropout(f=f, domain=domain, subspace_dim_size=5, fill_in_strategy=fill_in_strategy, maximize=True)
-    method.run_optimization(max_iter=500)
+    method.run_optimization(max_iter=500, eps=0)
 
     dim = len(domain)
     fill_in_strategy = 'copy'
@@ -53,4 +53,4 @@ for i in range(30):
     fill_in_strategy = 'mix'
     f = GaussianMixtureFunction(dim=dim, mean_1=2, mean_2=3)
     method = Dropout(f=f, domain=domain, subspace_dim_size=5, fill_in_strategy=fill_in_strategy, maximize=True, mix=0.5)
-    method.run_optimization(max_iter=500)
+    method.run_optimization(max_iter=500, eps=0)
