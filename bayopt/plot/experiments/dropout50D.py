92948dd906aca0cc9ebfc0d9ce26a245f74658fc
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-06-09 16:24:31 +0900

    [update] exec files to describe experiments results;

---
+++ bayopt/plot/experiments/dropout50D.py
@@ -0,0 +1,24 @@
+from bayopt.plot.loader import load_experiments
+from bayopt.plot.staticplot import StaticPlot
+import numpy as np
+
+
+for fill in ['random', 'copy', 'mix']:
+    results = load_experiments(
+        function_name='Gaussian mixture function',
+        start=None,
+        end=None,
+        dim='50D',
+        feature=fill,
+        iter_check=501
+    )
+
+    results = results * -1
+
+    x_axis = np.arange(0, len(results))
+    plot = StaticPlot()
+    for n in range(results.shape[1]):
+        plot.add_data_set(x=x_axis, y=results[:, n])
+
+    plot.set_y(low_lim=0, high_lim=1)
+    plot.finish(option='50D_' + fill)
