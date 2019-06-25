8c5a58d6193b53f3debec776cc77f53feb81e310
yamaguchi <zuuuubo.tetsu@outlook.jp>
2019-06-25 15:15:30 +0900

    [update]

--- bayopt/plot/experiments/Gaussian mixture function/select30D_theta.py
+++ bayopt/plot/experiments/Gaussian mixture function/select30D_theta.py
@@ -6,16 +6,16 @@ from bayopt.plot.utils import plot_experiment_subspace_dimensionality
 
 plot_experiment_theta(
     function_name='Gaussian mixture function', dim='30D',
-    method='mix_select', created_at='2019-06-17 04:39:59')
+    method='copy_select', created_at='2019-06-23 08:43:33')
 
 plot_experiment_theta_histogram(
     function_name='Gaussian mixture function', dim='30D',
-    method='mix_select', created_at='2019-06-17 04:39:59')
+    method='copy_select', created_at='2019-06-23 08:43:33')
 
 plot_experiment_mask(
     function_name='Gaussian mixture function', dim='30D',
-    method='mix_select', created_at='2019-06-17 04:39:59')
+    method='copy_select', created_at='2019-06-23 08:43:33')
 
 plot_experiment_subspace_dimensionality(
     function_name='Gaussian mixture function', dim='30D',
-    method='mix_select', created_at='2019-06-17 04:39:59')
+    method='copy_select', created_at='2019-06-23 08:43:33')
